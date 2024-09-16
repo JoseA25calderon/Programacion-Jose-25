b69cc86b5bc8921df089681273768e4694bee3fd
JoseA25calderon <joandres.calderon@gmail.com>
2024-09-15 22:30:35 -0400

    Add files via upload

---
+++ #receta de arepas Dinamica Jose Andres C.py
@@ -0,0 +1,15 @@
+#receta de arepas Dinamica Jose Andres Calderon 
+#python 3.12.3 
+foo=input("cuales son los ingredientes de una arepa?\n")
+print("1 taza de agua, 1 taza de harina pan , 1 cucharadita de sal , 1 cucharada de aceite\n")
+foo=input("y cuales son los pasos para realizar la receta?\n")
+print("Paso 1 En un bol se vierte el agua , la harina, y la sal\n")
+print("Paso 2 Se mezcla hasta que quede uniforme\n")
+print("Paso 3 Se coloca con el aceite en un budare hasta dorar\n")
+print("Paso 4 Se voltea y repite\n")
+print("Paso 5 Se sirve en un plato\n")
+foo=input("cuanta masa de arepa resulta al final con esta receta y cuantas arepas serian?\n")
+print("siguiendo esa cantidad de ingredientes la receta da 300 gramos de masa de arepa lo que da como resultado 1 arepa grande o 2 pequeñas")
+           
+
+
